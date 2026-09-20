7f199577a4b14198f878d6ede66574fb9f6f48e0
jech57 <je.hernandezc1@uniandes.edu.co>
2026-09-20 18:58:20 -0500

    add baseline

---
+++ src/app/features/tournament-new/tournament-new.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-new',
+  imports: [],
+  templateUrl: './tournament-new.html',
+  styleUrl: './tournament-new.scss',
+})
+export class TournamentNew {
+
+}
